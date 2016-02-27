846e9b6b5d9e1a614d18c9f253f7419d31408245
Matt Levan <matt.s.levan@gmail.com>
2016-02-27 09:25:51 -0800

    Added Game.py

---
+++ Game.py
@@ -0,0 +1,11 @@
+'''
+Game.py
+
+An abstract base class that defines the attributes and methods
+required for a game implementation such as a tile game, 8-puzzle, or
+cannibal/missionary.
+
+'''
+
+from abc import ABCMeta # Metaclass required for creating an ABC.
+
