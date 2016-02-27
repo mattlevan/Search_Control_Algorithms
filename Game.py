f7339430b6a4067524c1712c05e5b1601aa1196f
Matt Levan <matt.s.levan@gmail.com>
2016-02-27 14:10:34 -0800

    Added Game attributes

--- Game.py
+++ Game.py
@@ -9,3 +9,8 @@ cannibal/missionary.
 
 from abc import ABCMeta # Metaclass required for creating an ABC.
 
+class Game(metaclass=ABCMeta):
+    def __init__(self, graph, start, goal):
+        self.graph = graph
+        self.start = start
+        self.goal = goal
