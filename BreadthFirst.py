50834c45737efa09c67efc771d70ba5db097cb40
Nolan Thompson <nolanruss@gmail.com>
2016-03-04 19:50:46 -0800

    BreadthFirst in progress, EightPuzzle working

--- BreadthFirst.py
+++ BreadthFirst.py
@@ -6,64 +6,89 @@ comparing to both the game goal state and all previous states visited.
 
 
 '''
-'''
-	def shortestpath(puzzle):
-		try:
-			return next(bfs_paths(puzzle))
-		except StopIteration:
-			return None
-
-shortest_path(game)
-from Game_list import EightPuzzle
-'''
 from Game_list import EightPuzzle
 
-game = EightPuzzle([])
-print('Start of game: ', game.GetStart())
-print('Goal of game: ', game.GetGoal())
+class BreadthFirst:	
+	'''
+		build_graph generates states and stores them in the graph{} 
+		dictionary until either the goal is reached, or a set number
+		of moves are performed.
+	'''
 
-start = game.GetStart()
-goal = game.GetGoal()
-graph = {}
-begin, children = game.Move(start)
-update, newchild = game.Move(children[1])
-print('Move x: ', update)
-print('begin, children', begin, children)
-print('update, newchild', update, newchild)
-graph.update({begin: children})
-graph.update({update: newchild})
-print('next move: ', graph)
+	def __init__(self):
+		print('bfs created')
 
-puzzle = EightPuzzle([])
-class BreadthFirst:
-	def bfs_paths(puzzle):
-		graph = {}
-		currentpath = set()
-		start = puzzle.GetStart()
-		goal = puzzle.GetGoal()
-		graph.update({goal: 'GOAL'})
-		queue = [(tuple(start), [start])]
+	def build_graph(self):
+		MAX_MOVES = 300
+		goal_found = False
+		while not goal_found:
+			# Generate a new game and get the start/goal states.
+			puzzle = EightPuzzle([]) 
+			start = puzzle.GetStart()
+			goal = tuple(puzzle.GetGoal())
+			graph = {} # Stores visited states as (Parent, children) pairs.
+			# Get the initial move information for dictionary.
+			parent, children = puzzle.Move(start)
+			graph.update({parent: children})
+			# For each key in the graph, add children to the dictionary
+			# and check if a goal state is reached
+			key = parent 
+			tempdict = {}
+			counter = 0
+			while counter < MAX_MOVES: 
+				for key in graph:
+					for vertex in graph[key]:
+						child = tuple(vertex)
+						print('child: ',child, ' vertex: ',vertex, '\nkey: ', key, 'graph[key]: ', graph[key])
+						# If the child is not in the dictionary, add it.
+						if child not in graph:
+							movestart, newstates = puzzle.Move(child)
+							print('movestart: ',movestart, ' states: ',newstates)
+							tempdict.update({movestart: newstates})
+					#print('key: ', key)
+				# If the goal is reached, set the goal_found boolean.
+				graph.update(tempdict)
+				print('updating dictionary')
+				for key in graph:
+					print(key)
+				if goal in graph:
+					print('Goal found!')
+					goal_found = True
+					break
+				counter += 1
+		return puzzle, graph	
+	'''
+		bfs first calls build_graph to generate a dictionary that
+		has reached the goal state. The method then searches each state
+		in the graph to find the shortest path to the goal. 
+
+	'''
+	def bfs(self):	
+		# Generate a game and graph containing the goal state.
+		legal_game, legal_graph = self.build_graph()
+		goal_state = legal_game.GetGoal()	
+		# Store the states not yet visited in queue.
+		visited, queue = set(), legal_game.GetStart() 
+		
+		# While the queue is not empty, perform the search.
 		while queue:
-			# Store the first move in vertex, path
-			(vertex,currentpath) = queue.pop() 
-			# Add the vertex and path to the dictionary "graph"
-			if vertex not in graph:
-				movestart, newstates = puzzle.Move(vertex)
-				graph.update({movestart: newstates})
-			for next in graph[vertex]: 
-				if next in currentpath:
+			(vertex, path) = queue.pop(0) # Pop the current graph key.
+			
+			# For each child of a vertex/key compare to the states seen.
+			for next in graph[vertex]:
+				# If already visited state, skip to next child state.
+				if next in path:
 					continue
-				# Test if the path is the goal state.
-				if next == graph[goal]:
-					print('Goal state found!')
-					return currentpath	
+				# Otherwise, check if found the goal state.
+				elif next == graph.GetGoal():
+					# Yield the path followed to obtain the goal.
+					yield path.append(next)		
 				else:
-					currentpath.append([next])
-					queue.append((next, currentpath))
-			# Append the paths to the current path
-			# Pop a path from the beginning of nyv.
-			# Test if the path is present in graph.
-			# Test if the path is the goal state.
-			# If the path is not present, then perform a move
-x = BreadthFirst
-y = x.bfs_paths(game)
+					path.append(next)
+					queue.append((next, path))	
+
+newgame = EightPuzzle([])
+bfsearch = BreadthFirst()
+list1 = list(bfsearch.bfs())
+
+
--- Game_list.py
+++ Game_list.py
@@ -20,7 +20,7 @@ class EightPuzzle:
 # 3 4 5
 # 6 7 8
 	def __init__(self, start):
-		self.goal = [0,1,2,3,4,5,6,7,8]
+		self.goal = [1,2,3,8,0,4,7,6,5]
 		if(start == []):
 			print('Start is empty')
 			self.start = [0,1,2,3,4,5,6,7,8]
